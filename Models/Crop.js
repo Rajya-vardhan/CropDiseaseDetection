d1dcfb7faa2c10c4d08cf63454a56e6697a2e276
PMSV RAJYAVARDHAN <rajyavardhan28125.com@gmail.com>
2024-08-31 16:15:59 +0530

    version 2

--- Models/Crop.js
+++ Models/Crop.js
@@ -5,7 +5,9 @@ const cropSchema=new mongoose.Schema(
     {
         DiseasePic: {type:String,required:true} ,// stores url of the image
         DiseaseDesc:{type:String},
-        DiseaseDiagnosis:{type:String}
+        DiseaseDiagnosis:{type:String},
+        Confidence:{type:Number}
+
         
     }
 )
